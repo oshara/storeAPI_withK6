e4a58a9598b5b5548d4062524752b7ef7b824d38
oshara <amanthi1995@gmail.com>
2026-05-02 19:27:59 +0530

    adding thrshold configs

---
+++ thresholds/thresholds.js
@@ -0,0 +1,7 @@
+export const thresholds1 ={
+
+   'group_duration{group::: Login Flow}': ['p(95) < 200'],
+   'group_duration{group::: Products Flow}': ['p(95) <800'],
+   'group_duration{group::: Users Flow}': ['p(90) < 400']
+
+}
